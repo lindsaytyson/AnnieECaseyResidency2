3820977f9af4fd1dcefe7ed208258ba7b47186bc
lindsaytyson <lindsaytyson@Lindsays-MacBook-Air.local>
2015-05-24 23:55:46 -0400

    AnnieECaseyV8

--- myalscript.js
+++ myalscript.js
@@ -8,7 +8,7 @@ $(function () {
             spacingBottom: 30
         },
         title: {
-            text: 'Maryland Economic Well-Being Data',
+            text: 'Alabama Economic Well-Being Data',
             style: {"fontFamily":"'Garamond'", fontWeight: 800,"fontSize":'22px'}
         },
         xAxis: {
@@ -35,32 +35,32 @@ $(function () {
         series: [
             {
             name: '% Teens Not in School or Working',
-            data: [0, 0, 0, 8, 8, 8, 8, 8], 
+            data: [0, 0, 0, 10, 10, 11, 10, 10], 
             color: '#6fb574'
             }, {
             name: ' ',
-            data: [0, 0, 0, 9, 6, 4, 3, 4],
+            data: [0, 0, 0, 10, 5, 3, 5, 5],
             color: 'white',
         },{
             name: '% Children with No Employed Parent',
-            data: [0, 0, 0, 21, 24, 26, 27, 26],
+            data: [0, 0, 0, 30, 35, 37, 35, 35],
             color: '#39823e'
         },{
             name: ' ',
-            data: [9, 10, 10, 10, 8, 7, 6, 6],
+            data: [10, 12, 11, 13, 10, 7, 7, 8],
             color: 'white',
             
         },{
             name: '% Children in Poverty',
-            data: [11, 10, 10, 10, 12, 13, 14, 14],
+            data: [25, 23, 24, 22, 25, 28, 28, 27],
             color: '#1c5b20'
         },{
             name: ' ',
-            data: [10, 9, 5, 4, 3, 3, 4, 8],
+            data: [13, 10, 10, 10, 7, 5, 5, 8],
             color: 'white',
         },{
             name: '% Children in Homes Costing >30% of Income',
-            data: [35, 36, 40, 41, 42, 42, 41, 37],
+            data: [27, 30, 30, 30, 33, 35, 35, 32],
             color: '#072e0a' 
         }]
     });
@@ -76,7 +76,7 @@ $(function () {
             spacingBottom: 30
         },
         title: {
-            text: 'Maryland Education Data',
+            text: 'Alabama Education Data',
             style: {"fontFamily":"'Garamond'", fontWeight: 800, "fontSize":'22px'}
         },
         xAxis: {
@@ -102,31 +102,31 @@ $(function () {
         series: [
             {
             name: '% H.S. Students Not Graduating On Time',
-            data: [0, 20, 20, 20, 20, 18, 16, 16, 16], 
+            data: [0, 34, 33, 31, 30, 28, 24, 25, 0], 
             color: '#9c5a9c'
             }, {
             name: ' ',
-            data: [0, 7, 9, 9, 11, 11, 10, 60, 72],
+            data: [0, 6, 8, 8, 8, 7, 6, 65, 0],
             color: 'white'
         },{
             name: '% Children Ages 3-4 Not In Preschool',
-            data: [0, 53, 51, 51, 49, 49, 50, 0, 0],
+            data: [0, 59, 57, 57, 57, 58, 59, 0, 0],
             color: '#843284'
         },{
             name: ' ',
-            data: [5, 75, 12, 75, 15, 75, 15, 75],
+            data: [5, 90, 8, 90, 10, 90, 10, 90],
             color: 'white'
         },{
             name: '% 8th Graders < Proficient Math',
-            data: [70, 0, 63, 0, 60, 0, 60, 0, 63],
+            data: [85, 0, 82, 0, 80, 0, 80, 0, 80],
             color: '#660066'
         },{
             name: ' ',
-            data: [7, 75, 11, 75, 12, 75, 18, 75, 20],
+            data: [7, 85, 14, 85, 13, 85, 16, 85, 16],
             color: 'white'
         },{
             name: '% 4th Graders < Proficient Reading',
-            data: [68, 0, 64, 0, 63, 0, 57, 0, 55],
+            data: [78, 0, 71, 0, 72, 0, 69, 0, 69],
             color: '#470047' 
         }]
     });
@@ -143,7 +143,7 @@ $(function () {
             
         },
         title: {
-            text: 'Maryland Health Data',
+            text: 'Alabama Health Data',
             style: {"fontFamily":"'Garamond'", fontWeight: 800,"fontSize":'22px'}
         },
         xAxis: {
@@ -169,31 +169,31 @@ $(function () {
         series: [
             {
             name: '% Teens Who Have Abused Alcohol or Drugs',
-            data: [0, 7, 8, 7, 6, 6, 6, 6], 
+            data: [0, 7, 7, 7, 7, 7, 6, 6], 
             color: '#71acc9'
             }, {
             name: ' ',
-            data: [0, 15.6, 15.9, 5, 5, 5, 5, 6],
+            data: [0, 10, 10, 2, 4, 4, 5, 6],
             color: 'white'
         },{
             name: '% Children Without Health Insurance',
-            data: [0, 0, 0, 5, 5, 5, 5, 4],
+            data: [0, 0, 0, 8, 6, 6, 5, 4],
             color: '#447ea9'
         },{
             name: ' ',
-            data: [0, 0, 0, 5.8, 5.9, 6.2, 6.1, 6.2],
+            data: [4.3, 4.5, 4.6, 4.4, 4.7, 4.7, 5.1, 5.0],
             color: 'white'
         },{
             name: '% Low-Birthweight Babies',
-            data: [9.1, 9.4, 9.1, 9.2, 9.1, 8.8, 8.9, 8.8],
+            data: [10.7, 10.5, 10.4, 10.6, 10.3, 10.3, 9.9, 10.0],
             color: '#306994'
         },{
             name: ' ',
-            data: [10, 9, 6, 9, 15, 16, 40, 40],
+            data: [7, 5, 8, 10, 13, 13, 50, 50],
             color: 'white'
         },{
             name: 'Child and Teen Deaths Per 100,000',
-            data: [30, 31, 34, 31, 25, 24, 0, 0],
+            data: [43, 45, 42, 40, 37, 37, 0, 0],
             color: '#00214f' 
         }]
     });
@@ -210,7 +210,7 @@ $(function () {
     
         },
         title: {
-            text: 'Maryland Family & Community Data',
+            text: 'Alabama Family & Community Data',
             style: {"fontFamily":"'Garamond'", fontWeight: 800,"fontSize":'22px'}
         },
         xAxis: {
@@ -236,23 +236,23 @@ $(function () {
         series: [
             {
             name: '% Children with Household Head Lacking H.S. Diploma',
-            data: [10, 10, 10, 10, 10, 10, 10, 10], 
+            data: [17, 17, 16, 16, 15, 16, 15, 14], 
             color: '#ffc891'
             }, {
             name: ' ',
-            data: [8, 8, 7, 7, 6, 4, 3, 4],
+            data: [8, 8, 7, 9, 6, 5, 6, 6],
             color: 'white'
         },{
             name: '% Children in Single Parent Families',
-            data: [32, 32, 33, 33, 34, 36, 37, 36],
+            data: [37, 37, 38, 36, 39, 40, 39, 39],
             color: '#ff9237'
         },{
             name: ' ',
-            data: [8, 6, 6, 7, 9, 13, 15, 18],
+            data: [10, 6, 6, 7, 9, 16, 19, 21],
             color: 'white'
         },{
             name: 'Teen Births Per 1,000 Females',
-            data: [32, 34, 34, 33, 31, 27, 25, 22],
+            data: [50, 54, 54, 53, 51, 44, 41, 39],
             color: '#ff6d2e'
         }]
     });
